a46907bcdde088cafbf3e9eb6d56b28b9c9b1eda
Eldar-svg <natsu9888@gmail.com>
2024-10-19 03:05:12 +0400

    Removed useCallback Hook

--- src/useAxios.jsx
+++ src/useAxios.jsx
@@ -1,68 +1,34 @@
-import { useState } from "react";
 import axios from "axios";
 import { Todocontext } from "./useReduceStates";
 import { useContext } from "react";
-import { useCallback } from "react";
 
 const useAxios = () => {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
 
   const { state, dispatch } = useContext(Todocontext);
-
+  const todoes = state.todo
   const setTodo = "setTodo";
 
-  const handleRequest = (callback) => {
-    setLoading(true);
-    setError(null);
-    try {
-      callback();
-    } catch (err) {
-      setError(err);
-    } finally {
-      setLoading(false);
-    }
+  const getDataByCallback = async (url) => {
+    const { data } = await axios(url);
+    dispatch({ type: setTodo, payload: data });
   };
 
-  const getDataByCallback = useCallback(
-    (url) => {
-      handleRequest(async () => {
-        const response = await axios(url);
-        dispatch({ type: setTodo, payload: response.data });
-      });
-    },
-    [dispatch]
-  );
+  const postDataByCallback = async (url, newData) => {
+    const {data} = await axios.post(url, newData);
+    const newTodo = { ...data, check: false };
+    dispatch({ type: setTodo, payload: [newTodo, ...todoes] });
+  };
 
-  const postDataByCallback = useCallback(
-    (url, newData) => {
-      handleRequest(async () => {
-        const response = await axios.post(url, newData);
-        const newTodo = { ...response.data, check: false };
-        dispatch({ type: setTodo, payload: [newTodo, ...state.todo] });
-      });
-    },
-
-    [dispatch, state.todo]
-  );
-
-  const deleteDataByCallback = useCallback(
-    (url, _id) => {
-      handleRequest(async () => {
-        await axios.delete(`${url}/${_id}`);
-        dispatch({
-          type: setTodo,
-          payload: state.todo.filter(({ id }) => id !== _id),
-        });
-      });
-    },
-
-    [dispatch, state.todo]
-  );
+  const deleteDataByCallback = async (url, _id) => {
+    await axios.delete(`${url}/${_id}`);
+    dispatch({
+      type: setTodo,
+      payload: todoes.filter(({ id }) => id !== _id),
+    });
+  };
 
   return {
-    loading,
-    error,
+  
     getDataByCallback,
     postDataByCallback,
     deleteDataByCallback,
