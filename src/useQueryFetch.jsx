1afde0c4a9579b78c98f0d6ba4f1e2080d06960d
Eldar-svg <natsu9888@gmail.com>
2024-10-27 18:20:53 +0400

    Created a new component

---
+++ src/useQueryFetch.jsx
@@ -0,0 +1,75 @@
+import useAxios from "./useAxios";
+import useToggleHook from "./useToggleHook";
+import { useQuery, useQueryClient, useMutation } from "react-query";
+function useQueryFetch() {
+    const {inputState } = useToggleHook({
+        title: "",
+        description: "",
+        fresh: "",
+        search: "",
+        img: "",
+        ingredients: "",
+        openSearch: false,
+        openForm: false,
+        CloseModal: true,
+        OpenModal: false,
+      });
+  const queryClient = useQueryClient();
+  const {
+    isLoading,
+    isError,
+    refetch: Refresh,
+  } = useQuery("token", DataAxsios, { enabled: false });
+
+  const { getDataByCallback, postDataByCallback, deleteDataByCallback } =
+    useAxios();
+
+  function DataAxsios() {
+    getDataByCallback("https://api.sampleapis.com/coffee/iced");
+  }
+
+  const postMutation = (newTodo) =>
+    postDataByCallback("https://api.sampleapis.com/coffee/iced", newTodo);
+  const { mutate:newData } = useMutation(postMutation, {
+    onSuccess: () => {
+      queryClient.invalidateQueries("token");
+    },
+  });
+
+  const handlePost = (e) => {
+    e.preventDefault();
+    const newTodo = {
+      id: crypto.randomUUID(),
+      title: inputState.title,
+      ingredients: inputState.ingredients
+        .split(",")
+        .map((ingredient) => ingredient.trim())
+        .filter((ingredient) => ingredient !== ""),
+      description: inputState.description,
+      image: inputState.img,
+    };
+
+    newData(newTodo);
+  };
+
+  const deleteMutation = (id) =>
+    deleteDataByCallback(`https://api.sampleapis.com/coffee/iced`, id);
+  const { mutate: Data } = useMutation(deleteMutation, {
+    onSuccess: () => {
+      queryClient.invalidateQueries("token");
+    },
+  });
+
+  const deleteQuery = (id) => {
+    Data(id);
+    console.log(Data);
+  };
+
+  const fethcAgain = () => {
+    Refresh();
+  };
+
+  return {DataAxsios,handlePost,deleteQuery,fethcAgain,isLoading,isError};
+}
+
+export default useQueryFetch;
