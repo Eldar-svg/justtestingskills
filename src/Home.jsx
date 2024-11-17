9704f20779cfd4e1b50f0d32ee10ef33daaedbc3
Eldar-svg <natsu9888@gmail.com>
2024-11-17 16:09:35 +0400

    no changes

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,35 +1,51 @@
-import React from 'react'
-import Root from './Root'
+import React from "react";
+import Root from "./Root";
 
 function Home() {
   return (
-
-    <div className="App"><Root/>
-    <div style={{width:"50%", margin:"auto" }}>Welcome to Café YOU! 
-    
-    Cafe YOU logo w RESTAURANTCafe YOU Hours: Monday to Saturday: 7:45 AM to 3:30 PM. Kitchen closes at 3 PM. If you are coming with a group of more than 6 people please make a reservation – 239-600-0966.     
-    
-    1423 SE 10th Street, Unit 1A, Cape Coral 
-    
-    
-    Café YOU is an Australian style cafe that serves simple, natural food – including Vegan and Gluten Free options – accompanied by authentic espresso coffee. Most coffee shops in the United States don’t do much with food and most restaurants don’t worry about coffee. The Australian café is a hybrid combining a strong coffee culture and a robust food culture under one roof in a welcoming, cozy and comfortable neighborhood café.
-    
-    Our Coffee – We serve a range of espresso coffees made from the finest coffee beans, roasted right here in Cape Coral, and our baristas really know their beans. With their superior technical knowledge and a passion for making amazing coffee they will make you the perfect cuppa.
-    
-    Our Food – Our food is all natural, simple food made with local ingredients, wherever possible, to provide you with fresher, healthier and deliciously tasty meals. We have a well stocked pastry cabinet, our food has an international flavor and we offer gluten free, vegetarian and vegan options.
-    
-    Café YOU Events & Private Functions – We host monthly wine dinners, chefs’ tasting dinners and more. We also enjoy hosting your special occasions – whether baby showers, anniversaries, birthdays, or office parties. See our Fine Dining & Functions page for more details.
-    
-    We also have a little attitude: Café YOU has a cool, trendy shabby chic decor and an upbeat fun atmosphere with a whole lot of colorful idioms: brekkie (breakfast), avo (avocado), and “no worries” (you’re welcome).
-    
-    It’s the perfect place to relax on your own or to catch up with friends while enjoying great coffee and great food.
-    
-    Like in most Australian cafés one of our signature menu items is our “avocado smash”. Sometimes called an avocado toast, the perfect comfort food for any time of the day. We smash half a ripe avocado and some feta cheese onto a thick piece of toast (of your choice), season it with salt, pepper and chili flakes (optional), then give it a splash of olive oil and a squeeze of lemon.
-    
-    In Australia, cafés play a central role in people’s lifestyle. Its where people hang out, where they meet up with friends and have business meetings; before work, after a morning swim or working out, for breakfast, for lunch, a snack or just a coffee.
-    
-    We hope Café YOU will become part of your neighborhood and lifestyle.</div></div>
-  )
+    <div className="App">
+      <Root />
+      <div style={{ width: "50%", margin: "auto" }}>
+        Welcome to Café YOU! Cafe YOU logo w RESTAURANTCafe YOU Hours: Monday to
+        Saturday: 7:45 AM to 3:30 PM. Kitchen closes at 3 PM. If you are coming
+        with a group of more than 6 people please make a reservation –
+        239-600-0966. 1423 SE 10th Street, Unit 1A, Cape Coral Café YOU is an
+        Australian style cafe that serves simple, natural food – including Vegan
+        and Gluten Free options – accompanied by authentic espresso coffee. Most
+        coffee shops in the United States don’t do much with food and most
+        restaurants don’t worry about coffee. The Australian café is a hybrid
+        combining a strong coffee culture and a robust food culture under one
+        roof in a welcoming, cozy and comfortable neighborhood café. Our Coffee
+        – We serve a range of espresso coffees made from the finest coffee
+        beans, roasted right here in Cape Coral, and our baristas really know
+        their beans. With their superior technical knowledge and a passion for
+        making amazing coffee they will make you the perfect cuppa. Our Food –
+        Our food is all natural, simple food made with local ingredients,
+        wherever possible, to provide you with fresher, healthier and
+        deliciously tasty meals. We have a well stocked pastry cabinet, our food
+        has an international flavor and we offer gluten free, vegetarian and
+        vegan options. Café YOU Events & Private Functions – We host monthly
+        wine dinners, chefs’ tasting dinners and more. We also enjoy hosting
+        your special occasions – whether baby showers, anniversaries, birthdays,
+        or office parties. See our Fine Dining & Functions page for more
+        details. We also have a little attitude: Café YOU has a cool, trendy
+        shabby chic decor and an upbeat fun atmosphere with a whole lot of
+        colorful idioms: brekkie (breakfast), avo (avocado), and “no worries”
+        (you’re welcome). It’s the perfect place to relax on your own or to
+        catch up with friends while enjoying great coffee and great food. Like
+        in most Australian cafés one of our signature menu items is our “avocado
+        smash”. Sometimes called an avocado toast, the perfect comfort food for
+        any time of the day. We smash half a ripe avocado and some feta cheese
+        onto a thick piece of toast (of your choice), season it with salt,
+        pepper and chili flakes (optional), then give it a splash of olive oil
+        and a squeeze of lemon. In Australia, cafés play a central role in
+        people’s lifestyle. Its where people hang out, where they meet up with
+        friends and have business meetings; before work, after a morning swim or
+        working out, for breakfast, for lunch, a snack or just a coffee. We hope
+        Café YOU will become part of your neighborhood and lifestyle.
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
