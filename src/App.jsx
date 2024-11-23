29bcf92eb9aba092b1124a46560f1e8076b52406
Eldar-svg <natsu9888@gmail.com>
2024-11-23 18:30:38 +0400

    Break the code to a new component

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import { Outlet } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import { Todocontext } from "./useReduceStates";
 import { useContext, useState } from "react";
-import Modal from "./Modal";
+
 import useQueryFetch from "./useQueryFetch";
 import usePages from "./usePages";
 import { ToastContainer } from "react-toastify";
 
 import "react-toastify/dist/ReactToastify.css";
+import MainFunck from "./MainFunck";
 
 function App() {
   const { state, dispatch } = useContext(Todocontext);
@@ -43,6 +44,8 @@ function App() {
     CloseModal: true,
     OpenModal: false,
   });
+
+
 
   const toCloseModal = () => inputToggle("CloseModal");
 
@@ -88,85 +91,21 @@ function App() {
       />
 
       <Root />
-      {logdata === "admin" && (
-        <>
-          <button onClick={toCloseModal}>Add new coffee</button>
-          <Modal closeModal={toCloseModal} showModal={inputState.CloseModal}>
-            <form
-              onSubmit={(e) => {
-                handlePost(inputState, e);
-                handlerinput("title", "");
-                handlerinput("description", "");
-                handlerinput("ingredients", "");
-                toCloseModal();
-              }}
-            >
-              <p>Title:</p>
-              <input
-                value={inputState.title}
-                onChange={(e) => handlerinput("title", e.target.value)}
-                type="text"
-              />
-              <p>Description:</p>
-              <input
-                value={inputState.description}
-                onChange={(e) => handlerinput("description", e.target.value)}
-                type="text"
-              />
-              <p>Image:</p>
-              <input
-                type="url"
-                value={inputState.img}
-                onChange={(e) => addImg(e.target.value)}
-              />
-
-              <p>Ingred:</p>
-              <input
-                value={inputState.ingredients}
-                onChange={(e) => handlerinput("ingredients", e.target.value)}
-                type="text"
-              />
-
-              <p>
-                <button type="submit">Add</button>
-              </p>
-            </form>
-          </Modal>
-        </>
-      )}
-
-      <div>
-        <button onClick={() => inputToggle("openSearch")}>
-          {inputState.openSearch ? "close" : "Search bar"}
-        </button>
-        {inputState.openSearch ? (
-          <input
-            value={inputState.search}
-            onChange={(e) => handlerinput("search", e.target.value)}
-            type="text"
-            placeholder="enter the searchable text"
-          />
-        ) : null}
-      </div>
-
-      <button onClick={fethcAgain}>
-        {state.todo.length === 0 ? "Request Data" : "Refresh"}
-      </button>
-      <form>
-        <label htmlFor="d">
-          Choose All:
-          <input
-            type="checkbox"
-            id="d"
-            checked={state.allSelected}
-            onChange={selectAllBtn}
-          />
-       {  logdata === "admin" && <button onClick={deleteAll}>Clear</button>}
-        </label>
-      </form>
+      <MainFunck
+        inputToggle={inputToggle}
+        handlePost={handlePost}
+        handlerinput={handlerinput}
+        addImg={addImg}
+        selectAllBtn={selectAllBtn}
+        logdata={logdata}
+        toCloseModal={toCloseModal}
+        fethcAgain={fethcAgain}
+        deleteAll={deleteAll}
+        inputState={inputState}
+      />
       <Outlet />
       {selectedIngrid.map(({ id, ingredients, check }) => (
-        <p key={id}>
+        <p style={{ display: "flex", width: "30%", margin: "auto" }} key={id}>
           <input
             checked={check}
             onChange={(e) => toggleCheck(id, e.target.checked)}
@@ -175,7 +114,18 @@ function App() {
           {`${ingredients}`}
         </p>
       ))}
-      <ul style={{ listStyle: "none" }}>
+      <ul
+        style={{
+          display: "flexbox",
+          gap: "50px",
+
+          margin: "auto",
+          listStyle: "none",
+          marginTop: "50px",
+
+          alignItems: "stretch",
+        }}
+      >
         {currentItems
           .filter((prevSearch) => {
             const searchUpperCase = inputState.search.toLowerCase();
@@ -192,18 +142,35 @@ function App() {
           })
           .map(({ id, title, description, ingredients, image, check }) => {
             return (
-             <><li key={id}>
-             <input
-               type="checkbox"
-               checked={check}
-               onChange={(e) => toggleCheck(id, e.target.checked)}
-             />
-             <NavLink to={`/products/${id}`}>{title}</NavLink>
-             <p> {description}</p>
-             <p>{ingredients}</p>
-             <img src={image} alt={title} width="300" />
-             {logdata === "admin" && <button onClick={() => deleteQuery(id)}>Delete</button>}
-           </li></> 
+              <>
+                <li style={{ width: "50%" }} key={id}>
+                  <input
+                    type="checkbox"
+                    checked={check}
+                    onChange={(e) => toggleCheck(id, e.target.checked)}
+                  />
+                  <NavLink to={`/products/${id}`}>{title}</NavLink>
+                  <p> {description}</p>
+                  <p>{ingredients}</p>
+                  <img
+                    style={{
+                      width: "300px",
+                      height: "300px",
+                    }}
+                    src={image}
+                    alt={title}
+                    width="300"
+                  />
+                  {logdata === "admin" && (
+                    <button
+                      style={{ display: "flex" }}
+                      onClick={() => deleteQuery(id)}
+                    >
+                      Delete
+                    </button>
+                  )}
+                </li>
+              </>
             );
           })}
       </ul>
