86fcfa06cc6b581990f978f8bfe7e6d52b1ba38f
Eldar-svg <natsu9888@gmail.com>
2024-12-05 18:15:23 +0400

    New Folder

---
+++ src/mainstructure/Products/AddNewItem.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Modal from "./Modal";
+import InputofModal from "./InputofModal";
+
+function AddNewItem({
+  handlePost,
+  handlerinput,
+  addImg,
+  logdata,
+  toCloseModal,
+  inputState,
+}) {
+  return (
+    <div>
+      {logdata === "admin" && (
+        <>
+          <button onClick={toCloseModal}>Add new coffee</button>
+          <Modal closeModal={toCloseModal} showModal={inputState.CloseModal}>
+            <InputofModal
+              toCloseModal={toCloseModal}
+              handlePost={handlePost}
+              handlerinput={handlerinput}
+              addImg={addImg}
+              inputState={inputState}
+            />
+          </Modal>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default AddNewItem;
---
+++ src/mainstructure/Products/InputofModal.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Inputs from "./Inputs";
+function InputofModal({
+  handlePost,
+  handlerinput,
+  addImg,
+  inputState,
+  toCloseModal,
+}) {
+  const { title, description, img, ingredients } = inputState;
+  const onChangeInput = (input) => (e) => handlerinput(input, e.target.value);
+
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          handlePost(inputState, e);
+          handlerinput("title", "");
+          handlerinput("description", "");
+          handlerinput("ingredients", "");
+          toCloseModal();
+        }}
+      >
+        <Inputs
+          onChangeInput={onChangeInput}
+          addImg={addImg}
+          title={title}
+          description={description}
+          ingredients={ingredients}
+          img={img}
+        />
+      </form>
+    </div>
+  );
+}
+
+export default InputofModal;
