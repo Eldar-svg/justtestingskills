9be950dab8704ee3f0555441aa27862bb9068d83
Eldar-svg <natsu9888@gmail.com>
2025-01-07 22:51:18 +0400

    added new link

--- src/mainstructure/Products/coffe-list/EachCoffe.jsx
+++ src/mainstructure/Products/coffe-list/EachCoffe.jsx
@@ -1,6 +1,5 @@
 import { NavLink } from "react-router-dom";
 
-
 function EachCoffe({
   id,
   title,
@@ -13,46 +12,39 @@ function EachCoffe({
   toggleCheck,
 }) {
   return (
- 
-      <div>
-        <ul
-          style={{
-            listStyle: "none",
-            marginTop: "100px",
-          }}
-        >
-          <li
-            style={{ gap: "50px", margin: "auto", width: "50%" }}
-            key={id}
-          >
-            <input
-              type="checkbox"
-              checked={check}
-              onChange={(e) => toggleCheck(id, e.target.checked)}
-            />
-            <NavLink to={`/products/${id}`}>{title}</NavLink>
-            <p>{description}</p>
-            <p>{ingredients}</p>
-            <img
-              style={{
-                width: "300px",
-                height: "300px",
-              }}
-              src={image}
-              alt={title}
-            />
-            {logdata === "admin" && (
-              <button
-                style={{ display: "flex" }}
-                onClick={() => deleteQuery(id)}
-              >
-                Delete
-              </button>
-            )}
-          </li>
-        </ul>
-      </div>
-   
+    <div>
+      <ul
+        style={{
+          listStyle: "none",
+          marginTop: "100px",
+        }}
+      >
+        <li style={{ gap: "50px", margin: "auto", width: "50%" }} key={id}>
+          <input
+            type="checkbox"
+            checked={check}
+            onChange={(e) => toggleCheck(id, e.target.checked)}
+          />
+          <NavLink to={`/products/${id}`}>{title}</NavLink> 
+          <NavLink to={`/products/edit/${id}`}><button>Edit</button></NavLink>   
+          <p>{description}</p>
+          <img
+            style={{
+              width: "300px",
+              height: "300px",
+            }}
+            src={image}
+            alt={title}
+          />
+          <p>{ingredients}</p>
+          {logdata === "admin" && (
+            <button style={{ display: "flex" }} onClick={() => deleteQuery(id)}>
+              Delete
+            </button>
+          )}
+        </li>
+      </ul>
+    </div>
   );
 }
 
