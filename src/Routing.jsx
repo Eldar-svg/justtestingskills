676b69569c87185ea701aab639182bb385e18b33
Eldar-svg <natsu9888@gmail.com>
2025-01-07 22:47:15 +0400

    added product edit link

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,46 +1,47 @@
-import React from 'react'
+import React from "react";
 import ErrorPage from "./error-page";
 import LogIn from "./LogIn";
 import SinglePage from "./SinglePage";
 import Home from "./Home";
-import {QueryClient } from "react-query";
-import App from './App';
+import { QueryClient } from "react-query";
+import App from "./App";
+import EditCoffe from "./mainstructure/Products/coffe-list/EditCoffe";
 
-import {
-    createBrowserRouter,
-    Navigate,
-    
-  } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 function useRouting() {
-    const ProtectedRoute = ({ element }) => {
-        const isAuth = localStorage.getItem("token");
-        return isAuth ? element : <Navigate to="/login" />;
-      };
-      
-      const clientQ = new QueryClient()
-      
-      const router = createBrowserRouter([
-        {
-          path: "/login",
-          element: <LogIn />,
-          errorElement: <ErrorPage />,
-        },
-      
-        {
-          path: "/",
-          element: <ProtectedRoute element={<Home />} />,
-        },
-      
-        {
-          path: "/products",
-          element: <ProtectedRoute element={<App />} />,
-        },
-        {
-          path: "/products/:id",
-          element: <ProtectedRoute element={<SinglePage />} />,
-        },
-      ]);
-  return {clientQ,router}
+  const ProtectedRoute = ({ element }) => {
+    const isAuth = localStorage.getItem("token");
+    return isAuth ? element : <Navigate to="/login" />;
+  };
+
+  const clientQ = new QueryClient();
+
+  const router = createBrowserRouter([
+    {
+      path: "/login",
+      element: <LogIn />,
+      errorElement: <ErrorPage />,
+    },
+
+    {
+      path: "/",
+      element: <ProtectedRoute element={<Home />} />,
+    },
+
+    {
+      path: "/products",
+      element: <ProtectedRoute element={<App />} />,
+    },
+    {
+      path: "/products/:id",
+      element: <ProtectedRoute element={<SinglePage />} />,
+    },
+    {
+      path: "/products/edit/:id",
+      element: <ProtectedRoute element={<EditCoffe />} />,
+    },
+  ]);
+  return { clientQ, router };
 }
 
-export default useRouting
+export default useRouting;
