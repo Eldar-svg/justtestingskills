73e30a92bc2df9efb61e02f35ab674427598e8e5
Eldar-svg <natsu9888@gmail.com>
2025-05-24 20:39:53 +0400

    Layout for Outlet React Router

---
+++ src/Layout.tsx
@@ -0,0 +1,16 @@
+// Layout.tsx
+import { Outlet } from "react-router-dom";
+import { DataContext } from "./App"; // Или перемести контекст в отдельный файл
+
+const Layout = () => {
+  const logdata = localStorage.getItem("role");
+
+
+  return (
+    <DataContext.Provider value={{ logdata }}>
+      <Outlet />
+    </DataContext.Provider>
+  );
+};
+
+export default Layout;
