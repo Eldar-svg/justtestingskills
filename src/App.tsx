96fe360de4306e5cabd52991d1cdcda9ab17a533
Eldar-svg <natsu9888@gmail.com>
2025-05-24 20:40:12 +0400

    removed context

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import Root from "./Root";
 import useToggleHook from "./hooks/useToggleHook";
-import { Outlet } from "react-router-dom";
+
 import { useRef, useState, createContext } from "react";
 import useQueryFetch from "./hooks/useQueryFetch";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import MainFunck from "./mainstructure/Products/Produt/MainFunck";
- 
+import Layout from "./Layout";
 import MainCoffeList from "./mainstructure/Products/coffe-list/MainCoffeList";
 import Pages from "./mainstructure/Products/coffe-list/Pages";
 import useFetchHooks from "./hooks/useFetchHooks";
@@ -15,39 +15,44 @@ import { ListofToggleHook } from "./mainstructure/Products/Produt/ListofToggleHo
 export interface DataContextStart {
   // Массив булевых значений
   logdata: string | null;
-  deleteQuery: ((id: string) => void) | null; // Исправлено для согласованности
-  CheckToggle: (id: string, value: boolean) => void;
+
 }
 
 // Создание контекста
 export const DataContext = createContext<DataContextStart>({
+  logdata: null
 
-  logdata: null,
-  deleteQuery: null,
-  CheckToggle: () => {},
 });
 
 function App(): JSX.Element {
   const [page, setPage] = useState<number>(1);
- // Добавлено состояние для check
-  const { handlePost, deleteQuery, fetchAgain,CheckToggle,handleAll,BtnDelete } = useQueryFetch();
+  // Добавлено состояние для check
+  const {
+    handlePost,
+    deleteQuery,
+    fetchAgain,
+    CheckToggle,
+    handleAll,
+    BtnDelete,
+  } = useQueryFetch();
   const { ingredientBox, addImg } = useFetchHooks();
   const reff = useRef<HTMLDivElement | null>(null);
   const logdata = localStorage.getItem("role");
 
-  const { inputToggle, inputState, handlerinput } = useToggleHook<ListofToggleHook>({
-    id:"",
-    title: "",
-    description: "",
-    fresh: "",
-    image: "",
-    search: "",
-    ingredients: "",
-    openSearch: false,
-    openForm: false,
-    CloseModal: true,
-    OpenModal: false,
-  });
+  const { inputToggle, inputState, handlerinput } =
+    useToggleHook<ListofToggleHook>({
+      id: "",
+      title: "",
+      description: "",
+      fresh: "",
+      image: "",
+      search: "",
+      ingredients: "",
+      openSearch: false,
+      openForm: false,
+      CloseModal: true,
+      OpenModal: false,
+    });
 
   const handlerScrollUp = (pageNum: number): void => {
     setPage(pageNum);
@@ -69,10 +74,9 @@ function App(): JSX.Element {
         closeOnClick
         rtl={false}
       />
-
       <Root />
       <MainFunck
-      handlerScrollUp={handlerScrollUp}
+        handlerScrollUp={handlerScrollUp}
         inputToggle={inputToggle}
         handlePost={handlePost}
         handlerinput={handlerinput}
@@ -80,23 +84,13 @@ function App(): JSX.Element {
         logdata={logdata}
         toCloseModal={toCloseModal}
         fetchAgain={fetchAgain}
-     page={page}
+        page={page}
         inputState={inputState}
         CheckToggle={CheckToggle}
         handleAll={handleAll}
         BtnDelete={BtnDelete}
       />
-      <DataContext.Provider
-        value={{
-          
-          logdata,
-          deleteQuery,
-          CheckToggle
-        }}
-      >
-        <Outlet />
-      </DataContext.Provider>
-
+      <Layout />
       <MainCoffeList
         inputState={inputState}
         logdata={logdata}
@@ -105,8 +99,8 @@ function App(): JSX.Element {
         page={page}
         CheckToggle={CheckToggle}
       />
-
-      <Pages page={page} handlerScrollUp={handlerScrollUp} /> {/* Убрал setPage */}
+      <Pages page={page} handlerScrollUp={handlerScrollUp} />{" "}
+      {/* Убрал setPage */}
     </div>
   );
 }
