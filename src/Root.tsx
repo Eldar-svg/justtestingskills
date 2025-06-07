0e93991c66e422f37656838891d0d08c84c09176
Eldar-svg <natsu9888@gmail.com>
2025-06-07 04:37:03 +0400

    Clean up code

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,93 +1,55 @@
-import axios, { AxiosError } from "axios";
-import { useState, useEffect } from "react";
-import { useNavigate, Link } from "react-router-dom";
-
-interface FetchedData {
-  username: string;
-  role: string;
-}
-
-function Root():JSX.Element {
-  const navigate = useNavigate();
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
-  const [role, setRole] = useState<string>("");
-  const [username, setUsername] = useState<string>("");
-  const [errorMessage, setErrorMessage] = useState<string>("");
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    const storedRole = localStorage.getItem("role");
-
-    if (!token) {
-      setErrorMessage("Token not found, please log in again.");
-      return;
-    }
-
-    setIsLoggedIn(true);
-    setRole(storedRole || "");
-
-    // Получаем данные пользователя
-    const fetchUserData = async (): Promise<void> => {
-      try {
-        const response = await axios.get<FetchedData>(
-          "http://localhost:5000/current-user",
-          {
-            headers: {
-              Authorization: `Bearer ${token}`, // Отправляем токен
-            },
-          }
-        );
-        setUsername(response.data.username); // Устанавливаем имя пользователя
-      } catch (err) {
-        const error = err as AxiosError<{ message?: string }>;
-        setErrorMessage(error.response?.data?.message || "Error fetching user data. Please log in again.");
-        console.error("Error fetching user data:", error);
-      }
-    };
-
-    fetchUserData();
-  }, []);
-
-  const handleLogOut = (): void => {
-    localStorage.removeItem("token");
-    localStorage.removeItem("role");
-    setIsLoggedIn(false);
-    setRole("");
-    setUsername("");
-    navigate("/login");
-  };
+import { Link } from "react-router-dom";
+ 
+import useUserToken from "./hooks/useUserToken";
+ 
+function Root(): JSX.Element {
+  const { role, username, isLoggedIn, errorMessage, handleLogOut } =
+    useUserToken();
 
   return (
-    <div>
-      <ul
-        style={{
-          maxWidth: "100%", // Максимальная ширина
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          gap: "10px", // Отступы между элементами
-          flexWrap: "wrap", // Позволяет переносить элементы, если не помещаются
-          flexDirection: "row",
-          listStyle: "none",
-        } as React.CSSProperties}
-      >
-        <li>
-          <Link style={{ textDecoration: "none" }} to={`/`}>
-            About Us
-          </Link>
-        </li>
-        <li>
-          <Link style={{ textDecoration: "none" }} to={`/products`}>
-            Products
-          </Link>
-        </li>
-        <li>Contacts</li>
-        <h3>
-          Welcome {username || "guest"}! Role: {role}
-        </h3>
-        {isLoggedIn && <button onClick={handleLogOut}>Logout</button>}
-        {errorMessage && <div style={{ color: "red" }}>{errorMessage}</div>}
-      </ul>
+    <div className="w-full  ">
+      <div className=" flex  ">
+        <ul className=" relative flex bg-opacity-[70%] bg-red-300 w-full p-1  mb-5 mt-10 gap-1 items-center ">
+          <li className="rootbtn">
+            <Link style={{ textDecoration: "none" }} to={`/`}>
+              About Us
+            </Link>
+          </li>
+          <li className="rootbtn">
+            <Link style={{ textDecoration: "none" }} to={`/products`}>
+
+              Products
+            </Link>
+          </li>
+          <li className="rootbtn">Contacts</li>
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-[600px] max-w-[200px] shadow-xl ">
+            <img 
+              src="./coffee-shop-logos-design-template-cda8575e146cc5ba17cd3c1a24d65ba1_screen.jpg"
+              alt="alt"
+              className="rounded-[600px]  max-w-[240px] mt-9 shadow-xl"
+            />
+          </div>
+          {isLoggedIn && (
+            <li className=" text-center rounded-lg shadow-lg bg-white m-5 p-5 text-black ml-auto gap-1">
+              Welcome {username || "guest"}! <p>You Have role: {role}</p>
+            </li>
+          )}
+          {errorMessage && <div className="text-black">{errorMessage}</div>}
+
+          <button
+            onClick={handleLogOut}
+            className="bg-green-400 text-black hover:bg-red-400 p-5 m-5 rounded-lg shadow-xl"
+          >
+            {isLoggedIn ? (
+              "Logout"
+            ) : (
+              <Link style={{ textDecoration: "none" }} to={`/login`}>
+                Log In
+              </Link>
+            )}
+          </button>
+        </ul>
+      </div>
     </div>
   );
 }
