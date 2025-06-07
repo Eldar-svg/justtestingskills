6013c7861136f3fb4c5011c3228086d2b9fe4087
Eldar-svg <natsu9888@gmail.com>
2025-06-07 04:38:16 +0400

    installing tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "coffee-s":
+          "url('https://www.shutterstock.com/image-vector/brown-repeated-beans-coffee-pattern-600nw-2395113073.jpg')",
+      },
+      fontFamily: {
+        oswald: ["Oswald", "sans-serif"],
+      }
+    },
   },
   plugins: [],
 };
