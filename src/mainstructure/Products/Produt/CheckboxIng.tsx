05e0c27d7b7785254e9c4a75fc5df5b0fdc22e01
Eldar-svg <natsu9888@gmail.com>
2025-06-09 04:14:22 +0400

    tailwind and added clear in search

--- src/mainstructure/Products/Produt/CheckboxIng.tsx
+++ src/mainstructure/Products/Produt/CheckboxIng.tsx
@@ -28,38 +28,49 @@ function CheckboxIng({ CheckToggle }: CheckBoxResult): JSX.Element {
   if (error) return <div>Error occurred</div>;
 
   return (
-    <div className="flex space-x-2">
+    <div className="flex gap-2">
       <button
         className="arrow hover:-translate-x-1"
         disabled={pageIng === 1}
         onClick={(e) => handlerPageIng(e, pageIng - 1)}
       >
-
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 fill-indigo-500">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18" />
-</svg>
-
-
-      </button>
-      {data?.goods?.map(({ id, ingredients, check }: Product) => (
-        <label className="flex justify-center items-center flex-colm"
-          key={id}         
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-6 fill-indigo-500"
         >
-          <input 
-            type="checkbox"
-            checked={check ?? false}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              CheckToggle(id, e.target.checked)
-            }
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18"
           />
+        </svg>
+      </button>
+      <div className=" flex flex-col ">
+        {data?.goods?.map(({ id, ingredients, check }: Product) => (
+          <label
+            className=" flex justify-start  p-1 text-xl gap-3 underline"
+            key={id}
+          >
+            <input
+              type="checkbox"
+              checked={check ?? false}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                CheckToggle(id, e.target.checked)
+              }
+            />
 
-          {ingredients
-            ? Array.isArray(ingredients)
-              ? ingredients.join(", ")
-              : ingredients
-            : ""}
-        </label>
-      ))}
+            {ingredients
+              ? Array.isArray(ingredients)
+                ? ingredients.join(", ")
+                : ingredients
+              : ""}
+          </label>
+        ))}
+      </div>
       <button
         className="arrow"
         disabled={pageIng === data?.totalPages}
--- src/mainstructure/Products/Produt/SearchBar.tsx
+++ src/mainstructure/Products/Produt/SearchBar.tsx
@@ -6,31 +6,44 @@ interface SearchProps {
   inputState: ListofToggleHook;
   handlerinput: (field: string, value: InputValues) => void;
   inputToggle: (key: keyof ListofToggleHook) => void;
-  
 }
 function SearchBar({
   inputToggle,
   handlerinput,
   inputState,
- 
 }: SearchProps): JSX.Element {
   const { openSearch, search } = inputState;
   return (
     <div>
-      <motion.button whileTap={{ scale: 0.9 }} className="mainbtn" onClick={(): void => inputToggle("openSearch")}>
+      <motion.button
+        whileTap={{ scale: 0.9 }}
+        className="mainbtn"
+        onClick={(): void => inputToggle("openSearch")}
+      >
         {openSearch ? "close" : "Search bar"}
       </motion.button>
       {openSearch ? (
-        <input 
-        className=" ml-1 p-2 "
-          value={search}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            handlerinput("search", e.target.value)
-          }
-          type="text"
-          placeholder="enter the searchable text"
-        />
-      ) : null}
+        <p className="flex flex-col items-center m-2 ">
+          <input
+            className=" ml-1 p-2 min-w-[300px]"
+            value={search}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              handlerinput("search", e.target.value)
+            }
+            type="text"
+            placeholder="enter the searchable text"
+          />{" "}
+          <motion.button
+          className="p-2"
+            onClick={(e: React.MouseEvent<HTMLButtonElement>) =>
+              handlerinput("search", "")
+            }
+            whileTap={{ scale: 0.9 }}
+          >
+            Clear
+          </motion.button>{" "}
+        </p>
+      ) : null}{" "}
     </div>
   );
 }
