156fd5c5f76b15eb4699f39adf655787b99f4c56
Eldar-svg <natsu9888@gmail.com>
2025-06-22 04:29:59 +0400

    remove context

--- src/hooks/useFetchHooks.tsx
+++ src/hooks/useFetchHooks.tsx
@@ -1,33 +1,20 @@
-import { useContext, useState } from "react";
-import { Todocontext } from "./useReduceStates";
+import { useState } from "react";
+
 import useToggleHook from "./useToggleHook";
 
-
-
 export interface useFetchHooksResult {
- 
   ingredientBox: string[];
   addImg: (file: string) => void;
 }
 
 function useFetchHooks(): useFetchHooksResult {
- 
   const { handlerinput } = useToggleHook("");
 
-  const context = useContext(Todocontext);
-  if (!context) {
-    throw new Error("useFetchHooks must be used within a TodoProvider");
-  }
-
-
   const [ingredientBox] = useState<string[]>([]);
-
 
   const addImg = (file: string): void => {
     handlerinput("img", file);
   };
-
-
 
   return { ingredientBox, addImg };
 }
