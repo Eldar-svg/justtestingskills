154f00e552cb08e14c919398b5221b123d025bd8
Eldar-svg <natsu9888@gmail.com>
2025-07-21 05:09:11 +0400

    fix

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -5,8 +5,9 @@ module.exports = {
     extend: {
       backgroundImage: {
         "coffee-s":
-          "url('https://www.shutterstock.com/image-vector/brown-repeated-beans-coffee-pattern-600nw-2395113073.jpg')",
+          "url('/public/preview.jpg')",
       },
+      
       fontFamily: {
         oswald: ["Oswald", "sans-serif"],
       },
