90108485c409e9a887755305d1378e20a3b43f14
Eldar-svg <natsu9888@gmail.com>
2025-07-21 05:08:22 +0400

    new page

---
+++ Frontend/src/EditProfile.tsx
@@ -0,0 +1,12 @@
+ import React from 'react'
+ import Root from './Root'
+ const EditProfile: React.FC = () => {
+   return (
+    <>  <Root/>
+     <div className='bg-blue-500  flex justify-center max-w-[700px] mx-auto h-screen items-center text-2xl'>
+      EditProfile</div></>
+  
+   )
+ }
+ 
+ export default EditProfile
